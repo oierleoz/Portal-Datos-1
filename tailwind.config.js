fa75008a5ca7cb526eae83eeb94126aeefd790aa
oierleoz <oierleoz@gmail.com>
2026-05-26 11:52:47 +0200

    Update content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./*.{js,jsx}",
+    "./src/**/*.{js,jsx}"
+  ],
   theme: { extend: {} },
   plugins: [],
 }
